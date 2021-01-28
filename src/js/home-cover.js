11044154de61ee7b47cf3040016618141ab90514
Fernando Aquistapace <fernando.akistapace@gmail.com>
2021-01-28 14:33:27 -0300

    Iniciado inserção de tela no header e iniciado swiper da home

--- src/js/home-cover.js
+++ src/js/home-cover.js
@@ -1,16 +1,18 @@
 // import { Swiper, Navigation, Pagination, Autoplay } from 'swiper';
 // Swiper.use([Navigation, Pagination, Autoplay]);
 
-//     var swiper = new Swiper('.slide-home', {
-//         navigation: {
-//             nextEl: '.swiper-button-next',
-//             prevEl: '.swiper-button-prev',
-//         },
-//         pagination: {
-//             el: '.swiper-pagination'
-//         },
-//         mousewheel: true,
-//         keyboard: true,
-//     });
+    var swiper = new Swiper('.slide-home', {
+        speed: 400,
+        spaceBetween: 30,
+        grabCursor:true,
+        keyboard: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        pagination: {
+            el: '.swiper-pagination',
+        },
+    });
 
     
