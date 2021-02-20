8d4cb1d01464b80136bd43ce6af9301a270218fc
Fernando Aquistapace <fernando.akistapace@gmail.com>
2021-02-19 22:54:29 -0300

    Iniciado estrutura da seção de portfólio

--- src/js/gallery-portf.js
+++ src/js/gallery-portf.js
@@ -1,17 +1,27 @@
+var portfolio = new Swiper('.portfolio-slide', {
+  effect: 'coverflow',
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: 2.2,
+  watchSlidesVisibility: true,
+  watchSlidesProgress: true,
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 0,
+    depth: 100,
+    modifier: 1,
+    slideShadows: true,
+  },
+  pagination: {
+    el: '.swiper-pagination',
+  },
+});
+
 var galleryThumbs = new Swiper('.gallery-thumbs', {
-    spaceBetween: 10,
-    slidesPerView: 4,
-    freeMode: true,
-    watchSlidesVisibility: true,
-    watchSlidesProgress: true,
-  });
-  var galleryTop = new Swiper('.gallery-top', {
-    spaceBetween: 10,
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-    thumbs: {
-      swiper: galleryThumbs
-    }
-  });
+  spaceBetween: 10,
+  slidesPerView: 1,
+  freeMode: true,
+  thumbs: {
+    swiper: portfolio
+  }
+});
