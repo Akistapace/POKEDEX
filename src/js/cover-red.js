291cabb0ff465f4ceb5948448553e9531df26a5c
Fernando Aquistapace <fernando.akistapace@gmail.com>
2021-04-14 21:20:15 -0300

    Ajustes na capa de entrada, slider, botão azul do menu e cursor pokebola)

--- src/js/cover-red.js
+++ src/js/cover-red.js
@@ -3,16 +3,19 @@
     const triangulo = document.querySelector(".triangulo-yellow")
     const nav = document.querySelector(".nav")
     const body = document.body
+    const spanText = document.querySelector(".nav-content #btnOpen span")
 
     btnOpen.onclick =  ()=> { 
         if (sideNav.style.width == '100%') {
             sideNav.style.width = '0%'
             nav.style.position = 'absolute'
             body.style.overflow = 'auto'
+            spanText.style.position = 'initial'
         } else {
             sideNav.style.width = '100%'
             nav.style.position = 'fixed'
             body.style.overflow = 'hidden'
+            spanText.style.position = 'absolute'
         }
     }
 
@@ -20,11 +23,13 @@
         sideNav.style.width = '0%'
         nav.style.position = 'absolute'
         body.style.overflow = 'auto'
+        spanText.style.position = 'initial'
     };
     
     triangulo.onclick =  ()=> {
         sideNav.style.width = '0%'
         nav.style.position = 'absolute'
         body.style.overflow = 'auto'
+        spanText.style.position = 'initial'
     }
     
--- src/js/home-cover.js
+++ src/js/home-cover.js
@@ -1,16 +1,16 @@
 // import { Swiper, Navigation, Pagination, Autoplay } from 'swiper';
 // Swiper.use([Navigation, Pagination, Autoplay]);
 
-    var swiper = new Swiper('.slide-home', {
+    var swiper = new Swiper('.home-container-cover .slide-home', {
         speed: 400,
         grabCursor:true,
         keyboard: true,
         slidesPerView: 1,
         navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
+            nextEl: '.home-container-cover .button-next',
+            prevEl: '.home-container-cover .button-prev',
         },
         pagination: {
-            el: '.swiper-pagination',
+            el: '.home-container-cover .swiper-pagination',
         },
     });
