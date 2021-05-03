3a56c4ebdae525ec71bfb096d3554b943dd7d408
Fernando Aquistapace <fernando.akistapace@gmail.com>
2021-05-02 21:38:37 -0300

    integração mailchimp

--- src/js/contact.js
+++ src/js/contact.js
@@ -1,45 +1,17 @@
 const btnOpenContact = document.querySelector("#openContact")
-const containerContact = document.querySelector(".contact .contact-container")
-const modal = containerContact.querySelector('.contact-modal')
-const closeContact = document.querySelector(".close")
-
-const closeModal = ()=> {
-    modal.style.width = '0%'
-    modal.style.padding = '0%'
-    modal.style.overflowX = 'hidden';
-    setTimeout(() => {
-        containerContact.style.display = 'none'
-    }, 400);
-}
-const midiaQuerie = ()=> {
-    if (window.matchMedia("(max-width: 499px)").matches) { 
-        setTimeout(() => {
-            modal.style.width = 'calc(90% - 30px)'
-            modal.style.padding = '15px'
-            modal.style.overflow = 'initial'
-        }, 300);
-    } else {
-        setTimeout(() => {
-           modal.style.width = 'calc(100% - 30px)'
-           modal.style.padding = '15px'
-           modal.style.overflow = 'initial'
-        }, 300);
-    }
-}
+const closeContact = document.querySelector('.contact .close')
+const modal = document.querySelector('.Sendmail')
 
 btnOpenContact.onclick =  ()=> { 
-    if (containerContact.style.width == '100%') {
-        closeModal()
-    } else {
-        containerContact.style.display = 'block'
-        midiaQuerie()
-    }
+      modal.style.display = 'initial'
+      closeContact.style.display = 'initial'
 }
 closeContact.onclick = ()=> {
-    closeModal()
+    closeContact.style.display = 'none'
+    modal.style.display = 'none'
 }
-window.onclick = function(event) {
-    if (event.target == containerContact) {
-        closeModal()
-    }
-}
+
+closeContact.addEventListener('touchstart', ()=> {
+    closeContact.style.display = 'none'
+    modal.style.display = 'none'
+})
